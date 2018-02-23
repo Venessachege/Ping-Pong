e8b724d28f1ea1515e077f6f10373adfa3466f0a
venessachege <vchege.203@gmail.com>
2018-02-23 15:03:11 +0300

    Create a form to collect user input

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,35 @@
+function ping(input){
+  for (var i = 1; i <=input; i++) {
+    if ((i%3===0) && (i%5===0)){
+
+     $(".output").append("<li>"+ "pingpong"+"</li>");
+
+   }else if (i%3===0) {
+
+     $(".output").append("<li>"+"ping"+"</li>");
+   } else if (i%5===0) {
+
+     $(".output").append("<li>"+"pong"+"</li>");
+   }
+   else{
+    
+      $(".output").append("<li>"+i+"</li>");
+    }
+  }//end of for
+}
+//call the function
+
 $(document).ready(function(){
   //gather user input
-  var input=parseInt(prompt("Enter the number"));
+  //var input=parseInt(prompt("Enter the number"));
   //create a function
-  function ping(input){
-    for (var i = 1; i <=input; i++) {
-      if ((i%3===0) && (i%5===0)){
-       console.log("pingpong");
+  $("#form").submit(function(){
+    var input=$("#submit").val();
+    ping(input);
 
-     }else if (i%3===0) {
-       console.log("ping");
-     } else if (i%5===0) {
-       console.log("pong");
-     }
-     else{
-        console.log(i);
-      }
-    }//end of for
-  }
-  //call the function
-  ping(input);
+    event.preventDefault();
+  });
+
+
 
 });
