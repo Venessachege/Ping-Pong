1ba990efd7ac164fccce7426740bdf530c2be13a
venessachege <vchege.203@gmail.com>
2018-02-23 12:07:56 +0300

    Create the html structure

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  var input=parseInt(prompt("Enter the number"));
+  function ping(input){
+    for (var i = 1; i <=input; i++) {
+      if ((i%3===0) && (i%5===0)){
+       console.log("pingpong");
+
+     }else if (i%3===0) {
+       console.log("ping");
+     } else if (i%5===0) {
+       console.log("pong");
+     }
+     else{
+        console.log(i);
+      }
+    }//end of for
+  }
+  ping(input);
+
+});
